af0b1e641c6f6ae82d09b2bd59db35c47e090d59
Danny Cao <dannycao1997@yahoo.com>
2024-03-04 00:16:33 -0500

    added my name

--- src/main/java/com/github/zipcodewilmington/Wordguess.java
+++ src/main/java/com/github/zipcodewilmington/Wordguess.java
@@ -1,10 +1,39 @@
 package com.github.zipcodewilmington;
 //Danny Cao
 
+import java.util.Scanner;
+
+
 /**
  * @author xt0fer
  * @version 1.0.0
  * @date 5/27/21 11:02
  */
 public class Wordguess {
+    public static void main(String[] args) {
+        String words[] = {"cat", "dog", "bird", "fish"};
+        Wordguess game = new Wordguess();
+        game.runGame();
+    }
+
+
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
